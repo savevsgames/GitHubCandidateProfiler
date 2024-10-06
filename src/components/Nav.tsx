a6d7ebba3a56be2d495b066dc1d863c9589e88f0
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-06 08:07:31 -0600

    Navbar Working

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav">
+      <ul className="nav">
+        <Link to="/" className="nav-item nav-link">
+          <li>Home</li>
+        </Link>
+        <Link to="/SavedCandidates" className="nav-item nav-link">
+          <li>Saved Candidates</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
