7ad6260c337e5588e9459947f0269e879f2d1413
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-06 09:22:39 -0600

    Response mapping - added interface for Candidate

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -2,6 +2,84 @@ import { useState, useEffect } from "react";
 import { searchGithub, searchGithubUser } from "../api/API";
 import Candidate from "../interfaces/Candidate.interface";
 
+// export interface Candidate {
+//   name: string;
+//   username: string;
+//   location: string;
+//   avatar: string;
+//   email: string;
+//   html_url: string;
+//   company: string;
+// }
+
+// response for one user:
+// avatar_url
+// : 
+// "https://avatars.githubusercontent.com/u/14284298?v=4"
+// events_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/events{/privacy}"
+// followers_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/followers"
+// following_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/following{/other_user}"
+// gists_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/gists{/gist_id}"
+// gravatar_id
+// : 
+// ""
+// html_url
+// : 
+// "https://github.com/AndrewMangeni"
+// id
+// : 
+// 14284298
+// login
+// : 
+// "AndrewMangeni"
+// node_id
+// : 
+// "MDQ6VXNlcjE0Mjg0Mjk4"
+// organizations_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/orgs"
+// received_events_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/received_events"
+// repos_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/repos"
+// site_admin
+// : 
+// false
+// starred_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/starred{/owner}{/repo}"
+// subscriptions_url
+// : 
+// "https://api.github.com/users/AndrewMangeni/subscriptions"
+// type
+// : 
+// "User"
+// url
+// : 
+// "https://api.github.com/users/AndrewMangeni"
+
+const mapCandidate = (candidate: any): Candidate => {
+  return {
+    name: candidate.login,
+    username: candidate.login,
+    location: "",
+    avatar: candidate.avatar_url,
+    email: "",
+    html_url: candidate.html_url,
+    company: candidate.organizations_url,
+  };
+}
+
 const CandidateSearch = () => {
   const [candidates, setCandidates] = useState([]);
   const [lookupCandidate, setLookupCandidate] = useState("");
@@ -29,26 +107,37 @@ const CandidateSearch = () => {
     findCandidates();
   }, []);
 
+  const lookupCandidateHandler = async (username: string) => {
+    try {
+      const user = await searchGithubUser(username);
+      console.log(user);
+      setLookupCandidate(user);
+    } catch (error) {
+      setError("An error occurred while fetching the candidate's profile");
+      alert(error);
+    }
+  };
+
   return (
     <>
       <h1>CandidateSearch</h1>
       <main>
-        {candidates.map((candidate)=>{
+        {candidates.map((candidate) => {
+          const githubUser: Candidate = mapCandidate(candidate);
           return (
-            <div key={candidate.id}>
-              <h2>{candidate.login}</h2>
-              <img src={candidate.avatar_url} alt={candidate.login} />
+            <div key={githubUser.username}>
+              <h2>{githubUser.name}</h2>
+              <h3>{githubUser.username}</h3>
+              <img src={githubUser.avatar} alt={githubUser.username} />
               <button
-                onClick={async () => {
-                  const user = await searchGithubUser(candidate.login);
-                  console.log(user);
-                }}
+                onClick={() => lookupCandidateHandler(githubUser.username)}
               >
-                View Details
+                {githubUser.username}'s Profile
               </button>
             </div>
-          )
-        })}</main>;
+          );
+        })}
+      </main>
     </>
   );
 };
