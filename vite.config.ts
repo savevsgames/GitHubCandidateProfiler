68026105c3308da0bdfaf4030bfcc04b3140eb46
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-12 03:54:50 -0600

    vite config for render deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  },
   envDir: "./environment",
   plugins: [react()],
 });
